2dea5555e0b41c9e20ab52ce52d9720c00dafe9a
arwn <windham.aren@protonmail.com>
2024-10-21 17:22:20 -0500

    add fish

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
           environment.systemPackages = [
             pkgs.vim
             pkgs.jujutsu
+            pkgs.fish
 
             pkgs.mkalias # for making gui applications work
           ];
@@ -67,8 +68,8 @@
           nix.settings.experimental-features = "nix-command flakes";
 
           # Create /etc/zshrc that loads the nix-darwin environment.
-          programs.zsh.enable = true; # default shell on catalina
-          # programs.fish.enable = true;
+          # programs.zsh.enable = true; # default shell on catalina
+          programs.fish.enable = true;
 
           # Set Git commit hash for darwin-version.
           system.configurationRevision = self.rev or self.dirtyRev or null;
